b6d9dd7c40c9a78c410b0ef38f17f3726c2b7f2e
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-13 08:30:58 +0700

    Add criterion abstract class

---
+++ src/main/java/factory/Criterion.java
@@ -0,0 +1,25 @@
+package factory;
+
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <topsis> created by : 
+ * Name         : syafiq
+ * Date / Time  : 13 April 2017, 8:11 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Criterion<A extends Accumulator, W extends Weight, P extends Profit>
+{
+    public abstract void calculate(@NotNull A accumulator);
+
+    public abstract void normalize(@NotNull W accumulator);
+
+    public abstract void searchProfit(@NotNull P profit);
+
+    public abstract void searchLoss(@NotNull P profit);
+
+    public abstract void profitDistance(@NotNull P profit);
+
+    public abstract void lossDistance(@NotNull P profit);
+}
