27daab53bc3d2281757fe312acc29a36485cc408
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-13 10:46:44 +0700

    Case 0 : Add Properties instance

---
+++ case/src/main/java/case0/properties/Identity.java
@@ -0,0 +1,39 @@
+package case0.properties;
+
+import factory.Properties;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <topsis> created by : 
+ * Name         : syafiq
+ * Date / Time  : 13 April 2017, 8:51 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class Identity extends Properties
+{
+    @NotNull private String name;
+
+    public Identity(@NotNull String name)
+    {
+        this.name = name;
+    }
+
+    @NotNull public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(@NotNull String name)
+    {
+        this.name = name;
+    }
+
+    @Override public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("name", name)
+                .toString();
+    }
+}
