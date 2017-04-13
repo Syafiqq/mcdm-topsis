69aaf2c3682828b663e151910d4eb304d3781bda
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-13 10:45:09 +0700

    Case 0 : Add criterion Type

---
+++ case/src/main/java/case0/CriterionType.java
@@ -0,0 +1,14 @@
+package case0;
+
+/*
+ * This <topsis> created by : 
+ * Name         : syafiq
+ * Date / Time  : 13 April 2017, 8:42 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public enum CriterionType
+{
+    BENEFIT,
+    COST
+}
